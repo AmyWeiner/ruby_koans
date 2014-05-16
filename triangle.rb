4b62b481ac35dfeed2a60468cb98e6100e469bed
Amy Weiner <arweiner@alumni.stanford.edu>
2014-05-15 18:02:30 -0700

    Complete about triangle project

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,21 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError, "Sides must be greater than zero" if (a == 0) | (b == 0) | (c == 0)
+  raise TriangleError, "Sides must be postive" if (a < 0) | (b < 0) | (c < 0)
+  raise TriangleError, "The sum of two sides must be greater than the third side" if ((a + b) <= c) | ((a + c) <= b) | ((b + c) <= a)
+ 
+  if a == b && a == c
+    return :equilateral
+  end
+
+  if (a == b && a != c) || (b == c && b != a) || (c == a && c !=b)
+    return :isosceles
+  end
+
+  if a!= b && a!= c && b != c
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
